4e8fb1925e5eeeb322024e282ef17650dbeacc4b
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-02-16 15:07:41 -0500

    Exercise 14 Completed.

---
+++ LogicConcepts/Taylor/Program.cs
@@ -0,0 +1,37 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::");
+Console.WriteLine(":::::::: EJERCICIO SERIE DE TAYLOR ::::::::");
+Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::");
+do
+{
+    try
+    {
+        var n = ConsoleExtension.GetInt("Número de términos: ");
+        var x = ConsoleExtension.GetDouble("Valor de x........: ");
+        var taylor = Taylor(x, n);
+        Console.WriteLine($"f({x}) = {taylor:N5} ");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Desea continuar [S]í, [N]o?.....: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("::::: GAME OVER :::::");
+
+double Taylor(double x, int n)
+{
+    double sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += Math.Pow(x, i) / MyMath.Factorial(i);
+    }
+    return sum;
+}
