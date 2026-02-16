98d8cbe9cc6f74786c8528e4cb9956eac1c172ac
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-02-16 15:17:49 -0500

    Exercise 19 Completed.

---
+++ LogicConcepts/IsPrime/Program.cs
@@ -0,0 +1,27 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::");
+Console.WriteLine("::::::: EJERCICIO DE NÚMEROS PRIMOS :::::::");
+Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::");
+do
+{
+    try
+    {
+        var n = ConsoleExtension.GetInt("Ingrese número: ");
+        var isPrime = MyMath.IsPrime(n);
+        Console.WriteLine($"El número: {n} {(isPrime ? "SI" : "NO")} es primo.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Desea continuar [S]í, [N]o?.....: ", options);
+        Console.WriteLine();
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("::::: GAME OVER :::::");
