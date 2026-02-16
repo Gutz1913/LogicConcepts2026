cbc63b3b89fb957943853bfffcb463ec0adb29fc
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-02-16 15:20:52 -0500

    Exercise 21 Completed.

---
+++ LogicConcepts/ENumber/Program.cs
@@ -0,0 +1,48 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    try
+    {
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        Console.Clear();
+        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::");
+        Console.WriteLine(":::::::::::: EJERCICIO NÚMERO E :::::::::::");
+        Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::");
+        var n = ConsoleExtension.GetInt("Cuantos términos de precisión desea: ");
+
+        var e = CalculateE(n);
+
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.ForegroundColor = ConsoleColor.White;
+
+        Console.WriteLine($"El valor de 'e' con {n} términos de precisión es: {e}");
+
+        Console.BackgroundColor = ConsoleColor.Blue;
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Desea continuar [S]í, [N]o?..................: ", options);
+        Console.WriteLine();
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("::::: GAME OVER :::::");
+
+double CalculateE(int n)
+{
+    double sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += 1 / MyMath.Factorial(i);
+    }
+    return sum;
+}
