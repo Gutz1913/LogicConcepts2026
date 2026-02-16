2e08e17651b0244917220399854e9b707487ef5a
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-02-16 15:05:52 -0500

    Exercise 13 Completed.

---
+++ LogicConcepts/Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::");
+Console.WriteLine(":::::::::: EJERCICIO DE FACTORIAL :::::::::");
+Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::");
+do
+{
+    try
+    {
+        var n = ConsoleExtension.GetInt("Ingrese número: ");
+        var factorial = MyMath.Factorial(n);
+        Console.WriteLine($"{n}! = {factorial:N0}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Desea continuar [S]í, [N]o?.....: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("::::: GAME OVER :::::");
