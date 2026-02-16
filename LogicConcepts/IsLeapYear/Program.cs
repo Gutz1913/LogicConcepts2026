ca0eca2e97b7e5bebe04ef28dabc243cfdf25a9d
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-02-16 14:45:32 -0500

    Updates Shared Class

---
+++ LogicConcepts/IsLeapYear/Program.cs
@@ -0,0 +1,40 @@
+﻿var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
+Console.WriteLine("::::: EJERCICIO DE AÑO BISIESTO :::::");
+Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
+do
+{
+    var currentYear = DateTime.Now.Year;
+    var message = string.Empty;
+    var year = 0;
+    try
+    {
+        year = ConsoleExtension.GetInt("Ingrese el año..............: ");
+        if (year == currentYear)
+        {
+            message = "es";
+        }
+        else if (year > currentYear)
+        {
+            message = "va a ser";
+        }
+        else
+        {
+            message = "fue ";
+        }
+
+        var isLeapYear = DateUtilities.IsLeapYear(year);
+        Console.WriteLine($"El año {year}, {(isLeapYear ? "SI" : "NO")} {message} bisiesto");
+    }
+    catch (Exception)
+    {
+        Console.WriteLine($"Lo que ingresaste no es un año valido.");
+    }
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Desea continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("::::: GAME OVER :::::");
